f99c179793ae817349c2a0b10a27183a1107f39e
hyejeongjang <hj970219@naver.com>
2020-09-08 22:40:51 +0900

    fixed untracked files

--- config/database.js
+++
@@ -1,7 +0,0 @@
-// config/database.js
-module.exports = {
-  host: "wordballoon.clr4stjvntrc.ap-northeast-2.rds.amazonaws.com",
-  user: "admin",
-  password: "wordballoon20",
-  database: "wordballoon",
-};
