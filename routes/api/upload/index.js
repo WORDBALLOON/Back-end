9a66ab57d3f6a5c0040426a967e5dc5bfca641f8
hyejeongjang <hj970219@naver.com>
2020-09-10 03:56:36 +0900

    text rank 연결

--- routes/api/upload/index.js
+++ routes/api/upload/index.js
@@ -1,6 +1,22 @@
 const router = require("express").Router();
+const express = require("express");
 const controller = require("./upload.controller");
+const path = require("path");
+const multer = require("multer");
 
-router.post("/video", controller.video);
+let storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "upload/");
+  },
+  filename: function (req, file, callback) {
+    let extension = path.extname(file.originalname);
+    let basename = path.basename(file.originalname, extension);
+    callback(null, basename + "-" + Date.now() + extension);
+  },
+});
+
+var upload = multer({ storage: storage });
+
+router.post("/video", upload.single("videofile"), controller.video);
 
 module.exports = router;
--- routes/api/upload/upload.controller.js
+++ routes/api/upload/upload.controller.js
@@ -1,8 +1,10 @@
 const { Pvideo } = require("../../../models");
 const { Videokey } = require("../../../models");
-const express = require("express");
+const { PythonShell } = require("python-shell");
 
+const express = require("express");
 const multer = require("multer");
+var ffmpeg = require("fluent-ffmpeg");
 
 require("dotenv").config({ path: __dirname + "\\" + ".env" });
 
@@ -17,53 +19,82 @@ require("dotenv").config({ path: __dirname + "\\" + ".env" });
 */
 
 exports.video = async (req, res, next) => {
-    //res.send("video 라우터 작동중");
+  //res.send("video 라우터 작동중");
+  // submit 시, enctype="multipart/form-data"
 
-    var videofile = req.body.videofile;
-    var inbucket = req.body.inbucket;
-    var pvideotitle = req.body.pvideotitle;
+  // video 로컬에 저장
+  var videofile = req.file.videofile;
+  var inbucket = req.body.inbucket;
+  var pvideotitle = req.body.pvideotitle;
 
-    console.log(videofile);
-    console.log(inbucket);
-    console.log(pvideotitle);
+  //console.log(videofile);
+  console.log(inbucket);
+  console.log(pvideotitle);
 
+  let file = req.file;
 
-    // video 로컬에 저장
-    let storage = multer.diskStorage({
-        destination: (req, file, cb) => {                       //uploads 폴더에 저장
-            cb(null, 'uploads/')
-        },
-        filename: (req, file, cb) => {                          //파일 이름
-            cb(null, `${Date.now()}_${file.originalname}`)
-        },
-        fileFilter: (req, file, cb) => {                        //mp4형식만 가능
-            const ext = path.extname(file.originalname)
-            if (ext !== '.mp4') {
-                return cb(res.status(400).end('only mp4 is allowed'), false);
-            }
-            cb(null, true)
-        }
-    })
-    
-    const upload = multer({ storage: storage }).single("file")
-
-  
-    //썸네일 제작 : 혜정
-
-
-    //S3, 데이터베이스에 동영상이랑 썸네일 업로드
-
-
-    //파이썬 STT --> S3, 데이터베이스에 업로드 : 수진
-
-
-    //TextRank
-
-
-    //TextRank 값, STT Script 링크, 영상 링크, 썸네일 링크 데이터베이스에 저장
-
-
-    //res: 이미지 링크로 전송, textrank 리스크 값으로 전송
-
-
+  // 4. 파일 정보
+  let result = {
+    originalName: file.originalname,
+    size: file.size,
   };
+
+  console.log(file);
+
+  // test용 : res.json(result);
+
+  //썸네일 제작
+  // 제작된 썸네일 이름 : tn
+  ffmpeg("upload/" + file.filename).screenshots({
+    // 썸네일 제작 시, 영상의 20%를 가져와서 만든다.
+    count: 1,
+    filename: pvideotitle + ".png",
+    folder: "upload/",
+  });
+
+  //S3에 동영상이랑 썸네일 업로드
+
+  //파이썬 STT연결 --> S3 업로드
+
+  var options = {
+    mode: "text",
+    pythonPath: "",
+    pythonOptions: ["-u"],
+    scriptPath: "../server/stt",
+    args: [inbucket, pvideotitle],
+  };
+
+  // inbucket="en-us", "ko-kr"
+
+  PythonShell.run("translatevideo.py", options, function (err, results) {
+    if (err) console.log("err msg : ", err);
+    console.log("stt 전송 finished: %j", results);
+  });
+
+  //TextRank
+
+  var csv_filename = pvideotitle + "_sentence.csv";
+
+  var options = {
+    mode: "text",
+    pythonPath: "",
+    pythonOptions: ["-u"],
+    scriptPath: "../server/STT/Text_Rank",
+    args: csv_filename, // pvideotitle_sentence.csv
+  };
+  console.log("전달한 파일 이름" + csv_filename);
+
+  // 만약 영어 text-rank 돌려야 한다면
+  if (inbucket == "en-us") {
+    PythonShell.run("tr_eng.py", options, function (err, results) {
+      if (err) console.log("err msg : ", err);
+      console.log(results);
+    });
+  }
+
+  // 값이 안와요
+
+  //TextRank 값, STT Script 링크, 영상 링크, 썸네일 링크 데이터베이스에 저장
+
+  //res: 이미지 링크로 전송, textrank 리스크 값으로 전송
+};
